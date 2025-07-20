24d71229e853c4e601bede69dbd654bccbebea46
DeF4lt-Cap10N <captainakash1675@gmail.com>
2025-07-21 02:09:37 +0530

    added user and admin signup and signin funtion

--- routes/admin.js
+++ routes/admin.js
@@ -1,57 +1,84 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const adminRouter = Router();
 const bcrypt = require("bcrypt");
-const {adminModel} = require("../db")
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { adminModel } = require("../db")
 
 
-adminRouter.post("/signup", async(req, res)=>{
-    const {email, password, firstName, lastName} = req.body;
+adminRouter.post("/signup", async (req, res) => {
+    const { email, password, firstName, lastName } = req.body;
 
-    const hashPass =await bcrypt.hash(password, 5);
+    const hashPass = await bcrypt.hash(password, 5);
 
     await adminModel.create({
-        email:email,
-        password:hashPass,
-        firstName:firstName,
-        lastName:lastName
+        email: email,
+        password: hashPass,
+        firstName: firstName,
+        lastName: lastName
     })
 
-
     res.json({
-        messsage:"admin signup succesfully",
-        password:hashPass
+        messsage: "admin signup succesfully",
+        password: hashPass
     })
 
 
 })
 
-adminRouter.post("/signin", (req, res)=>{
+adminRouter.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+
+    const findAdmin = await adminModel.findOne({
+        email: email
+    })
+
+    const matchPass = bcrypt.compare(password, findAdmin.password);
+
+    console.log(matchPass);
+
+    if (matchPass) {
+        const token = jwt.sign({
+            id: findAdmin._id.toString(),
+        }, process.env.JWT_SECRET);
+
+        res.json({
+            token: token,
+            messsage: "login sucessfully"
+        })
+    }
+    else {
+        res.json({
+            messsage: "admin not find"
+        })
+    }
+
+
+})
+
+adminRouter.post("/course", (req, res) => {
     res.json({
-        messsage:""
+        messsage: ""
     })
 })
 
-adminRouter.post("/course", (req, res)=>{
+adminRouter.put("/course", (req, res) => {
     res.json({
-        messsage:""
-    })
-})
-
-adminRouter.put("/course", (req, res)=>{
-    res.json({
-        messsage:""
+        messsage: ""
     })
 })
 
 
-adminRouter.delete("/course", (req, res)=>{
+adminRouter.delete("/course", (req, res) => {
     res.json({
-        messsage:""
+        messsage: ""
     })
 })
 
 
 
 module.exports = {
-    adminRouter:adminRouter
+    adminRouter: adminRouter
 }
